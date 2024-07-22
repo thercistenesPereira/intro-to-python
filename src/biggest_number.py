07aec916c28da8384adf382469231912d7cc4efb
thercistenes <thercistenes1@outlook.com>
2024-07-21 21:18:26 -0300

    1 - Crie uma função que receba dois números e retorne o maior deles

--- src/biggest_number.py
+++ src/biggest_number.py
@@ -1,2 +1,6 @@
 def find_biggest_number(first_number: int, second_number: int) -> int:
-    raise NotImplementedError
+    # raise NotImplementedError
+    if first_number > second_number:
+        return first_number
+    else:
+        return second_number
