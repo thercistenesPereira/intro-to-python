4612aedf4d59a0ed70d535d07c8c4325d4831d75
thercistenes <thercistenes1@outlook.com>
2024-07-21 21:40:59 -0300

    2 - Calcule a média aritmética dos valores contidos em uma lista

--- src/average.py
+++ src/average.py
@@ -2,4 +2,8 @@ from typing import List
 
 
 def find_average(numbers: List[int]) -> float:
-    raise NotImplementedError
+    # raise NotImplementedError
+    total = 0
+    for number in numbers:
+        total += number
+    return total / len(numbers)
